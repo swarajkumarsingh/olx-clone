fbd7790584e9884db2b30e35d74a2b4e783a860c
swaraj <sswaraj169@gmail.com>
2023-11-10 00:01:03 +0530

    implemented: seller sql more optimized &  informative

--- migrations/scripts/20231026225733010339_sellers.sql
+++ migrations/scripts/20231026225733010339_sellers.sql
@@ -1,15 +1,24 @@
 CREATE TABLE IF NOT EXISTS sellers (
 	id SERIAL NOT NULL PRIMARY KEY,
-	username VARCHAR(500) UNIQUE NOT NULL,
-	fullname VARCHAR(100) NOT NULL,
-	email VARCHAR(100) NOT NULL,
-	password VARCHAR(200) NOT NULL,
-	number VARCHAR(10) NOT NULL,
-    rating NUMERIC(3, 2) CHECK (rating >= 0 AND rating <= 5),
-    description TEXT,
+
 	avatar TEXT,
-	location TEXT,
+    description TEXT,
+	password VARCHAR(200) NOT NULL,
+	fullname VARCHAR(100) NOT NULL,
     is_verified BOOLEAN DEFAULT FALSE,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	username VARCHAR(500) UNIQUE NOT NULL,
+
+	location TEXT,
 	coordinates TEXT,
+	phone VARCHAR(10) NOT NULL,
+
+	city VARCHAR(50) NOT NULL,
+	state VARCHAR(50) NOT NULL,
+	country VARCHAR(50) NOT NULL
+	zip_code VARCHAR(10) NOT NULL,
+
 	created_at TIMESTAMP DEFAULT NOW()
+    rating NUMERIC(3, 2) CHECK (rating >= 0 AND rating <= 5),
+	account_status ENUM('Active', 'Suspended', 'Banned') NOT NULL,
 );
